b216d956732de0a95a8e336b80432e38caa4a672
DeltCrosX <bitcoin.bkl.btc@gmail.com>
2023-04-10 05:49:46 +0700

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,16 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const myLoader = ({ src, quality }) => {
+  return `https://theredprod.cf/${src}?q=${quality || 75}`
+}
+
 export default function Home() {
   return (
     <main>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="./" className="flex items-center">
-            <Image src="https://theredprod.cf/src/BlueblockLogos.jpg" className="h-8 mr-3" alt="Blueblock Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Blueblock</span>
           </a>
           <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
